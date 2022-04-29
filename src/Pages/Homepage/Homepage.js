3eb7dbe0a5430c62808ce55d4c66101659e97ae3
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-04-29 20:23:10 +0600

    first section update

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,8 +1,40 @@
 import React from "react";
 
 function Homepage(props) {
-  return <div id="Homepage">
-  </div>;
+  return (
+    <div id="Homepage">
+      <Section1></Section1>
+    </div>
+  );
 }
 
 export default Homepage;
+
+const Section1 = () => {
+  return (
+    <section id="Section1">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="col_wrapper">
+              <h2 className="section_title">We are guarantee 100%.</h2>
+              <p className="para para_top">
+                You can make a contract by click bellow Create a contract
+                button.
+              </p>
+              <div className="btn_control">
+                <button>Create a contract</button>
+                <button>Watch Video</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="col_wrapper">
+              <img src={require("../../Static/img/we_are_img.png")} alt="img" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
